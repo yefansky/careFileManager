d366e978d615c32df32a0f3cdba235fbf55fae3a
yefan <yefansky@gmail.com>
2023-07-02 15:28:56 +0800

    角度旋转修正

--- careFileMgr.py
+++ careFileMgr.py
@@ -114,44 +114,91 @@ cases = [
 
 reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
 
+import matplotlib.pyplot as plt
+
 def rotate_image(image):
     # 转换为灰度图像
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # 边缘检测
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    edges = cv2.Canny(gray, 50, 100, apertureSize=3)
 
     # 检测直线
-    lines = cv2.HoughLines(edges, 1, np.pi/180, threshold=100)
-
-    if lines is not None and len(lines) >= 2:
-        # 计算旋转角度
-        angle = lines[0][0][1] * 180 / np.pi - 90
-        angle = np.clip(angle, -45, 45)  # 限制旋转角度在-45至45度之间
-
-        # 旋转图像
-        center = (image.shape[1] // 2, image.shape[0] // 2)
-        matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated = cv2.warpAffine(image, matrix, (image.shape[1], image.shape[0]))
-        return rotated
+    lines = cv2.HoughLines(edges, 1, np.pi/360, threshold=500)
     
-    return image
+    angle_sum = 0
+    avg_angle = 0
+    count = 0
+    
+    if lines is not None and len(lines) > 0:
+        for line in lines:
+            rho, theta = line[0]
+            angle = np.rad2deg(theta)
+            #print("raw angle=", angle)
+            if angle <= 90:
+                angle = 90 - angle
+            else:
+                angle = -(90 - (180 - angle))
+                
+            if angle > 45: 
+                angle -= 90
+            elif angle < -45:
+                angle += 90
+            
+        #print("process angle=", angle)
+        angle_sum += angle
+        count += 1
+        avg_angle = angle_sum / count
+    
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    #print("avg_angle = ", avg_angle)
+    '''
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)      
+    '''
+    matrix = cv2.getRotationMatrix2D(center, -avg_angle, 1.0)
+    rotated = cv2.warpAffine(image, matrix, (w, h))
+
+    # 显示图像    
+    image2 = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+    plt.imshow(image2)
+    plt.axis('off')  # 可选：关闭坐标轴
+    plt.show()
+
+    return rotated
 
 
 def Recognize(image):
     img = rotate_image(image)
     result = reader.readtext(img, detail=0)
     data = preprocessData(result)
+    print(data)
     output = process_cases(data, cases)
     print(output)
+    
+def singe_recognize(file_path):
+    print("Processing file:", file_path)
+    cv_img = cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), -1)
+    Recognize(cv_img)    
     
 def search_and_recognize(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.jpg'):
                 file_path = os.path.join(root, file)
-                print("Processing file:", file_path)
-                cv_img = cv2.imdecode(np.fromfile(file_path, dtype=np.uint8), -1)
-                Recognize(cv_img)
+                singe_recognize(file_path)
 
-search_and_recognize("I:\\Lab\\CareFileMgr")
+    
+singe_recognize("I:\\Lab\\CareFileMgr\\叶帆病历\\202306\\图像 (229).jpg")   
+#search_and_recognize("I:\\Lab\\CareFileMgr\\")
